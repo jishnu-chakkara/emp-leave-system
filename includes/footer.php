5e60f2eea7485fdcda088b64ff1bc16201edddad
Jvs Jishnu <jishnuvsmyr@gmail.com>
2023-04-19 19:46:30 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | Employee Leave Management System in PHP | Developed By <a href="https://kunjumol.com">Kunjumol Thomas</a></p>
+        </div>
+</footer>
